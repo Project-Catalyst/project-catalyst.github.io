08f46ed08001cbabdf41ab41484021bc367c297b
Michał Wojtera <michal.wojtera@gmail.com>
2021-03-17 16:54:24 +0100

    Fixed resource cards layout

--- src/.vuepress/components/ResourceCard.vue
+++ src/.vuepress/components/ResourceCard.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-card class="mx-auto" max-width="344" >
-    <v-card-title :style="'background-color:'+headerColor+'; color:white'">{{title}}</v-card-title>
+  <v-card class="mx-auto card-container" max-width="344">
+    <v-card-title :style="'background-color:'+headerColor+'; color:white'" >{{title}}</v-card-title>
     <v-card-subtitle :style="'background-color:'+headerColor+'; color:white'">{{subtitle}}</v-card-subtitle>
     <v-card-text>
       {{text}}
     </v-card-text>
-    <v-card-actions>
+    <v-card-actions class="bottom-cards">
       <v-btn :href="url" target="_blank">{{linkText}}</v-btn>
     </v-card-actions>
 </v-card>
@@ -44,3 +44,24 @@
     }
   }
 </script>
+<style>
+div.v-card__title{
+  white-space: nowrap;
+}
+div.v-card__subtitle{
+  white-space: nowrap;
+}
+div.v-card__text{
+  margin-top:1rem;
+}
+div.card-container{
+  position:relative;
+  padding-bottom:4rem;
+}
+div.bottom-cards{
+  position: absolute;
+  bottom:0;
+  margin-left:0.75rem;
+  margin-bottom:0.5rem;
+}
+</style>
