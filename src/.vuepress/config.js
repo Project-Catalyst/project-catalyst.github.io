4b204556e76d70638cb6fc3f8d21feb1a5de177e
Francisco Aramburo <francoara@pm.me>
2021-03-29 19:04:37 +0200

    Added support for il8n according https://vuepress.vuejs.org/guide/i18n.html#site-level-i18n-config

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -22,6 +22,21 @@ module.exports = {
   ],
   dest: 'html',
 
+  locales: {
+    // The key is the path for the locale to be nested under.
+    // As a special case, the default locale can use '/' as its path.
+    '/': {
+      lang: 'en-US', // this will be set as the lang attribute on <html>
+      title: 'Cardano Catalyst Community Site',
+      description: description
+    },
+    '/fr/': {
+      lang: 'fr-FR',
+      title: 'Cardano Catalyst Site Communautaire',
+      description: description
+    }
+  },
+
   /**
    * Theme configuration, here is the default theme configuration for VuePress.
    *
@@ -33,32 +48,92 @@ module.exports = {
     docsDir: '',
     editLinkText: '',
     lastUpdated: true,
-    nav: [
-      {
-        text: 'Proposers',
-        link: '/proposers/'
+    locales: {
+      '/': {
+        // text for the language dropdown
+        selectText: 'Languages',
+        // label for this locale in the language dropdown
+        label: 'English',
+        // Aria Label for locale in the dropdown
+        ariaLabel: 'Languages',
+        // text for the edit-on-github link
+        editLinkText: 'Edit this page on GitHub',
+        // config for Service Worker
+        serviceWorker: {
+          updatePopup: {
+            message: "New content is available.",
+            buttonText: "Refresh"
+          }
+        },
+        // algolia docsearch options for current locale
+        algolia: {},
+        nav: [
+          {
+            text: 'Proposers',
+            link: '/proposers/'
+          },
+          {
+            text: 'Voters',
+            link: '/voters/'
+          },
+          {
+            text: 'Community Advisors',
+            link: '/community-advisor/',
+          },
+          {
+            text: 'Funds',
+            link: '/funds/'
+          },
+          {
+            text: 'Resources',
+            link: '/resources/'
+          },
+          {
+            text: 'FAQ',
+            link: '/faq/'
+          },
+        ],
       },
-      {
-        text: 'Voters',
-        link: '/voters/'
+      '/fr/': {
+        selectText: 'Langages',
+        label: 'Français',
+        ariaLabel: 'Langages',
+        editLinkText: 'Modifier cette page sur GitHub ',
+        serviceWorker: {
+          updatePopup: {
+            message: "New content is available.",
+            buttonText: "Refresh"
+          }
+        },
+        algolia: {},
+        nav: [
+          {
+            text: 'Proposants',
+            link: '/fr/proposers/'
+          },
+          {
+            text: 'Les électeurs',
+            link: '/fr/voters/'
+          },
+          {
+            text: 'Conseillers communautaires',
+            link: '/fr/community-advisor/',
+          },
+          {
+            text: 'Fonds',
+            link: '/fr/funds/'
+          },
+          {
+            text: 'Ressources',
+            link: '/fr/ressources/'
+          },
+          {
+            text: 'FAQ',
+            link: '/fr/faq/'
+          },
+        ],
       },
-      {
-        text: 'Community Advisors',
-        link: '/community-advisor/',
-      },
-      {
-        text: 'Funds',
-        link: '/funds/'
-      },
-      {
-        text: 'Resources',
-        link: '/resources/'
-      },
-      {
-        text: 'FAQ',
-        link: '/faq/'
-      },
-    ]
+    },
   },
 
   /**
---
+++ src/fr/index.md
@@ -0,0 +1,25 @@
+---
+home: true
+heroImage: https://avatars.githubusercontent.com/u/79784766?s=400&u=51b65ef6f530a0d0bf4067deffe167c9cb2ce2cc&v=4
+tagline: Project Catalyst is a series of experiments which seeks to generate the highest levels of community innovation. Catalyst is bringing on-chain governance to the Cardano blockchain by allowing the community to self-determine priorities for growth. It also lets participants deploy funding to proposals which tackle challenges and capitalize on opportunities that arise in the life cycle of Cardano.
+
+actionText: Read more >
+actionLink: /docs/
+features:
+- title: Who is a Proposer?
+  details: Proposers are a vital aspect of Project Catalyst. They are the fuel to the fire. Proposers are the ideas people, ones that see the bigger picture, identify needs, plug gaps. A proposal made by a proposer is the way to communicate that idea to the community.
+
+- title: Who is a Voter?
+  details:   Proposers are a vital aspect of Project Catalyst. They are the fuel to the fire. Proposers are the ideas people ones that see the bigger picture, identify needs, plug gaps. A proposal made by a proposer is the way to communicate that idea to the community.
+
+- title: Who is a Community Advisor?
+  details: Proposers are a vital aspect of Project Catalyst. They are the fuel to the fire. Proposers are the ideas people ones that see the bigger picture, identify needs, plug gaps. A proposal made by a proposer is the way to communicate that idea to the community.
+
+
+footer: Made by Catalyst Community with ❤️
+---
+<span style="text-align:center">
+
+| [Resources for proposers](fr/proposers/README.md) | [Resources for voters](fr/voters/README.md) | [Resources for Community Advisors](fr/community-advisor/README.md) |
+
+</span>
