f93e19ad5d97492640224aebd2ee2d3c424ad756
Michał Wojtera <michal.wojtera@gmail.com>
2021-05-10 17:56:04 +0200

    Added heap.io tracking script

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -16,10 +16,13 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    [<script type="text/javascript">
-      window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
-      heap.load("4064924217");
-    </script>]
+    
+    ['script', { 'type':'text/javascript',content:'window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};'+
+        'var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";'+
+        'var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);,'+
+        'for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},'+
+        'p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],'+
+        'o=0;o<p.length;o++)heap[p[o]]=n(p[o])}; heap.load("4064924217");'}],
     ['meta', { name: 'theme-color', content: '#001D9D' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
